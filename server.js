a41a6526514441111dedbfdde69e7e7a53ce5b6e
JAMES ZHANG <jzhang300@us.ibm.com>
2016-04-20 14:45:27 -0500

    trigger travis

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ require('cf-deployment-tracker-client').track();
 var server = require('./app');
 var port = process.env.PORT || process.env.VCAP_APP_PORT || 3000;
 
+
 server.listen(port, function () {
   console.log('Server running on port: %d', port);
 });
