d503757f2686bae54bd3ee4d681783bcac8bb442
Alex Meyer <alexmeyer@us.ibm.com>
2016-12-16 14:06:39 -0600

    update file

--- public/js/components/App.js
+++ public/js/components/App.js
@@ -311,6 +311,7 @@ function App(documentTones, sentences, thresholds, selectedSample) { // eslint-d
     var map = function (item) {
       var result = item;
       result.className = _toneLevel(_selectedFilter, item.tone_categories[_searchIndex(_selectedTone)].tones[_searchIndex(_selectedFilter)].score, 'className_OT');
+r     esult.text = result.text.replace(/\r?\n/g, '<br />');
       return result;
     };
     return _originalSentences.map(map);
