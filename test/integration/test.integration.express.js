d3a0ca685cab5082e76e4eab7118613bd19a61fb
Richie Verma <richie.v@ibm.com>
2017-09-26 20:41:18 -0700

    Updated tests to execute when credentials are set

--- test/integration/test.integration.express.js
+++ test/integration/test.integration.express.js
@@ -32,95 +32,95 @@ var sampleTextReview = fs.readFileSync(filePathReview, 'utf-8');
 var filePathEmail = path.join(__dirname, '../..', '/public/data/personal-email.txt');
 var sampleTextEmail = fs.readFileSync(filePathEmail, 'utf-8');
 
-if (!process.env.TONE_ANALYZER_USERNAME || process.env.TONE_ANALYZER_USERNAME == '<username>') {
-  return;
+//Execute when credentials are set
+if (process.env.TONE_ANALYZER_USERNAME && process.env.TONE_ANALYZER_USERNAME != '<username>') {
+
+  describe('integration-express', function() {
+    this.timeout(15000);
+
+    it('Analyze tones in tweets sample', function() {
+      return request(app).post('/api/tone')
+        .type('form')
+        .send({
+          language: 'en',
+          source_type: 'text',
+          accept_language: 'en',
+          include_raw: false,
+          text: sampleTextTweets
+        }).expect(200);
+    });
+
+    it('Analyze tones in reviews sample', function() {
+      return request(app).post('/api/tone')
+        .type('form')
+        .send({
+          language: 'en',
+          source_type: 'text',
+          accept_language: 'en',
+          include_raw: false,
+          text: sampleTextReview
+        }).expect(200);
+    });
+
+    it('Analyze tones in email sample', function() {
+      return request(app).post('/api/tone')
+        .type('form')
+        .send({
+          language: 'en',
+          source_type: 'text',
+          accept_language: 'en',
+          include_raw: false,
+          text: sampleTextEmail
+        }).expect(200);
+    });
+
+    it('Analyze tones in own text', function() {
+      return request(app).post('/api/tone')
+        .type('form')
+        .send({
+          language: 'en',
+          source_type: 'text',
+          accept_language: 'en',
+          include_raw: false,
+          text: 'This is a chair'
+        }).expect(200);
+    });
+
+    it('Analyze tones for more than 1000 lines of input', function() {
+      return request(app).post('/api/tone')
+        .type('form')
+        .send({
+          language: 'en',
+          source_type: 'text',
+          accept_language: 'en',
+          include_raw: false,
+          text: sampleTextTweets.repeat(100)
+        }).expect(200)
+        .expect(function(res) {
+          if (!('warning' in res.body.document_tone)) {
+            throw new Error('Missing warning when there are more than 1000 sentences to analyze.');
+          }});
+    });
+
+    it('Generate Error when there is no text for analysis', function() {
+      return request(app).post('/api/tone')
+        .type('form')
+        .send({
+          language: 'en',
+          source_type: 'text',
+          accept_language: 'en',
+          include_raw: false,
+          text: ''
+        }).expect(500);
+    });
+
+    it('Analyze tones when only text is specified', function() {
+      return request(app).post('/api/tone')
+        .type('form')
+        .send({
+          text: sampleTextTweets
+        }).expect(200);
+    });
+
+  });
 }
-
-describe('integration-express', function() {
-  this.timeout(15000);
-
-  it('Analyze tones in tweets sample', function() {
-    return request(app).post('/api/tone')
-      .type('form')
-      .send({
-        language: 'en',
-        source_type: 'text',
-        accept_language: 'en',
-        include_raw: false,
-        text: sampleTextTweets
-      }).expect(200);
-  });
-
-  it('Analyze tones in reviews sample', function() {
-    return request(app).post('/api/tone')
-      .type('form')
-      .send({
-        language: 'en',
-        source_type: 'text',
-        accept_language: 'en',
-        include_raw: false,
-        text: sampleTextReview
-      }).expect(200);
-  });
-
-  it('Analyze tones in email sample', function() {
-    return request(app).post('/api/tone')
-      .type('form')
-      .send({
-        language: 'en',
-        source_type: 'text',
-        accept_language: 'en',
-        include_raw: false,
-        text: sampleTextEmail
-      }).expect(200);
-  });
-
-  it('Analyze tones in own text', function() {
-    return request(app).post('/api/tone')
-      .type('form')
-      .send({
-        language: 'en',
-        source_type: 'text',
-        accept_language: 'en',
-        include_raw: false,
-        text: 'This is a chair'
-      }).expect(200);
-  });
-
-  it('Analyze tones for more than 1000 lines of input', function() {
-    return request(app).post('/api/tone')
-      .type('form')
-      .send({
-        language: 'en',
-        source_type: 'text',
-        accept_language: 'en',
-        include_raw: false,
-        text: sampleTextTweets.repeat(100)
-      }).expect(200)
-      .expect(function(res) {
-        if (!('warning' in res.body.document_tone)) {
-          throw new Error('Missing warning when there are more than 1000 sentences to analyze.');
-        }});
-  });
-
-  it('Generate Error when there is no text for analysis', function() {
-    return request(app).post('/api/tone')
-      .type('form')
-      .send({
-        language: 'en',
-        source_type: 'text',
-        accept_language: 'en',
-        include_raw: false,
-        text: ''
-      }).expect(500);
-  });
-
-  it('Analyze tones when only text is specified', function() {
-    return request(app).post('/api/tone')
-      .type('form')
-      .send({
-        text: sampleTextTweets
-      }).expect(200);
-  });
-
-});
