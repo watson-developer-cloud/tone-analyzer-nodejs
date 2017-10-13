91867e6b03d399be9c8ac42e81f133b0a06dcc3b
Richie Verma <richie.v@ibm.com>
2017-10-12 21:11:04 -0700

    Updated default language to en

--- config/i18n.js
+++ config/i18n.js
@@ -20,20 +20,19 @@ var i18n = require('i18n');
 
 function initializeI18n(app){
   i18n.configure({
-    locales: [
-      'en', 'fr'
-    ],
+    locales: ['en', 'fr'],
     fallbacks: {
       'en-US': 'en',
       'fr-FR': 'fr',
     },
     directory: __dirname + '/../i18n',
-    defaultLocale: 'fr'
+    defaultLocale: 'en',
   });
 
   app.use(i18n.init);
   // eslint-disable-next-line
   console.log('i18n module initialized. Default locale: ' + i18n.getLocale());
+
 }
 
 module.exports = initializeI18n;
